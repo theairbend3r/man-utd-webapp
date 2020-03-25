55e97b7812d72b775b47a23b096a2f16c8781b7a
Akshaj Verma <akshajverma7@gmail.com>
2020-03-25 18:28:25 +0530

    Added mufc logo.

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ import styled from "@emotion/styled/macro"
 import { css } from "@emotion/core"
 import Head from "next/head"
 
+import MUFCLogo from "../public/MUFCLogo"
+
 const SidebarComponent = styled.div(
   tw`min-h-screen w-1/6 bg-gray-900 text-gray-100`
 )
 const MainpageComponent = styled.div(
   tw`min-h-screen w-5/6 bg-gray-100 text-gray-900`
 )
-
-const MyButton = styled.button(tw`font-bold text-lg p-2 bg-red-900`)
 
 const Index = () => {
   return (
@@ -20,7 +20,24 @@ const Index = () => {
         <title>Manchester United</title>
       </Head>
       <div tw="flex flex-wrap flex-row min-h-screen">
-        <SidebarComponent> SIDEBAR </SidebarComponent>
+        <SidebarComponent>
+          <div tw="flex flex-col justify-center h-full items-center justify-around">
+            <div>
+              <MUFCLogo />
+            </div>
+            <div>
+              <div> Home </div>
+              <div> About </div>
+              <div> Players </div>
+              <div> Trophies </div>
+              <div> News </div>
+            </div>
+            <div tw="px-6 text-xs text-gray-400 text-justify">
+              All copyrights belong to Manchester United Football Club and the
+              respective players.
+            </div>
+          </div>
+        </SidebarComponent>
         <MainpageComponent> MAINPAGE </MainpageComponent>
       </div>
     </div>
---
+++ public/MUFCLogo.js
@@ -0,0 +1,11 @@
+import tw from "twin.macro"
+
+const MUFCLogo = () => {
+  return (
+    <div tw="w-40 h-40">
+      <img src="/images/mufc_logo.jpg" />
+    </div>
+  )
+}
+
+export default MUFCLogo
