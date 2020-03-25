dbc440a4dda93ffed03d5405a079a36c645d0164
Akshaj Verma <akshajverma7@gmail.com>
2020-03-25 16:01:25 +0530

    Created base sidebar and mainpage components.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
-import tw from 'twin.macro'
-const Button = tw.button`text-lg px-8 py-2 rounded bg-red-500 `
-const SuccessButton = () => <Button>Success</Button>
+import React from "react"
+import tw from "twin.macro"
+import styled from "@emotion/styled/macro"
+import { css } from "@emotion/core"
+import Head from "next/head"
 
-export default SuccessButton
+const SidebarComponent = styled.div(
+  tw`min-h-screen w-1/6 bg-gray-900 text-gray-100`
+)
+const MainpageComponent = styled.div(
+  tw`min-h-screen w-5/6 bg-gray-100 text-gray-900`
+)
+
+const MyButton = styled.button(tw`font-bold text-lg p-2 bg-red-900`)
+
+const Index = () => {
+  return (
+    <div tw="mx-auto">
+      <Head>
+        <title>Manchester United</title>
+      </Head>
+      <div tw="flex flex-wrap flex-row min-h-screen">
+        <SidebarComponent> SIDEBAR </SidebarComponent>
+        <MainpageComponent> MAINPAGE </MainpageComponent>
+      </div>
+    </div>
+  )
+}
+
+export default Index
