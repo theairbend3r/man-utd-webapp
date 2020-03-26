45b454aa3727ec25b3ffb5f0076bc3b02b4f08b4
Akshaj Verma <akshajverma7@gmail.com>
2020-03-26 22:15:29 +0530

    Changed sidebar width proportion to 1/6th ofentire page.

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,11 +5,11 @@ import MUFCLogo from "../public/MUFCLogo"
 
 const Navbar = () => {
   return (
-    <div tw="flex flex-row sm:flex-col bg-gray-900">
+    <div tw="flex flex-row sm:flex-col sm:w-1/6 bg-gray-900">
       <div tw="self-center">
         <MUFCLogo />
       </div>
-      <div tw="flex flex-row flex-wrap justify-center items-center w-screen sm:flex-col sm:w-full sm:my-auto text-yellow-500">
+      <div tw="flex flex-row flex-wrap justify-center items-center w-screen sm:flex-col sm:w-full sm:my-auto text-yellow-500 text-2xl">
         <div tw="px-2 py-1"> Home </div>
         <div tw="px-2 py-1"> About </div>
         <div tw="px-2 py-1"> Players </div>
