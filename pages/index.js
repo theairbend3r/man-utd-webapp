3013e51a5048bdd1ef3586d7e3c472bda2d5a2c1
Akshaj Verma <akshajverma7@gmail.com>
2020-03-26 16:18:41 +0530

    Added responsive sidebar layout.

--- pages/index.js
+++ pages/index.js
@@ -4,26 +4,23 @@ import styled from "@emotion/styled/macro"
 import { css } from "@emotion/core"
 import Head from "next/head"
 
-import Sidebar from "../components/Sidebar"
-import RedDevilsLogo from "../public/RedDevilsLogo"
-
-const MainpageComponent = styled.div(
-  tw`min-h-screen w-5/6 bg-gray-100 text-gray-900`,
-  css`
-    background: linear-gradient(#9b2c2c, #1a202c);
-    * {
-      ${tw`mt-6`}
-    }
-  `
-)
-
 const Index = () => {
   return (
     <div tw="mx-auto">
-      <div tw="flex min-h-screen">
-        <Head>
-          <title>Manchester United</title>
-        </Head>
+      <Head>
+        <title>Manchester United</title>
+      </Head>
+
+      <div tw="flex flex-col sm:flex-row">
+        <div tw="flex-1 sm:flex-none sm:w-1/5 sm:min-h-screen bg-gray-900 text-gray-100 p-4">
+          This is the Navbar
+        </div>
+        <div tw="flex-1 min-h-screen sm:w-4/5 bg-green-100 text-gray-900 p-4">
+          <p tw="mb-3">
+            LMAO What LMAO WhatLMAO What LMAO WhatLMAO What LMAO WhatLMAO What
+            LMAO WhatLMAO What LMAO What
+          </p>
+        </div>
       </div>
     </div>
   )
