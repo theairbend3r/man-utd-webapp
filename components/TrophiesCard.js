11a78ef6a0b27d62a0759427380b2752298a62a9
Akshaj Verma <akshajverma7@gmail.com>
2020-03-29 23:12:33 +0530

    Made trophies to scroll in X.

--- components/TrophiesCard.js
+++ components/TrophiesCard.js
@@ -2,25 +2,40 @@ import tw from "twin.macro"
 import { css } from "@emotion/core"
 
 const TrophiesCard = props => {
-  const trophyImage = props.trophyImage
-  const trophyCount = props.trophyCount
-  const trophyName = props.trophyName
+  const {
+    trophyImage1,
+    trophyCount1,
+    trophyName1,
+    trophyImage2,
+    trophyCount2,
+    trophyName2
+  } = props
 
   return (
-    <div tw="w-full md:w-1/2 lg:w-1/3 px-2 my-2">
-      <div tw="bg-white">
-        <div tw="rounded p-1">
+    <div tw="flex flex-col flex-shrink-0 h-full mx-2">
+      <div tw="bg-white p-1 rounded m-auto h-48 w-full sm:h-56 md:h-64 lg:h-72">
+        <div>
           <img
-            tw="rounded"
-            src={require(`../public/images/trophies/${trophyImage}.jpg`)}
+            tw="h-40 sm:h-48 md:h-56 lg:h-64 rounded object-cover"
+            src={require(`../public/images/trophies/${trophyImage1}.jpg`)}
           />
         </div>
-        <div tw="text-center p-1">
+        <div tw="text-center rounded border-gray-100 border-2 font-semibold bg-gray-900 text-gray-100">
           <p>
-            <span tw="font-bold text-xl px-1">{trophyCount}</span>
-            <span tw="text-base p-1 rounded font-semibold text-center bg-gray-900 text-gray-100">
-              {trophyName}
-            </span>
+            {trophyCount1} {trophyName1}
+          </p>
+        </div>
+      </div>
+      <div tw="bg-white p-1 rounded m-auto h-48 w-full sm:h-56 md:h-64 lg:h-72">
+        <div>
+          <img
+            tw="h-40 sm:h-48 md:h-56 lg:h-64 rounded object-cover"
+            src={require(`../public/images/trophies/${trophyImage2}.jpg`)}
+          />
+        </div>
+        <div tw="text-center rounded border-gray-100 border-2 font-semibold bg-gray-900 text-gray-100">
+          <p>
+            {trophyCount2} {trophyName2}
           </p>
         </div>
       </div>
