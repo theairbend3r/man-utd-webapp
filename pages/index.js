51193707e6b6e93a9026a1701c981bbfd5172645
Akshaj Verma <akshajverma7@gmail.com>
2020-05-25 07:35:52 +0530

    removed aysnc function to fetch news.

--- pages/index.js
+++ pages/index.js
@@ -158,22 +158,22 @@ const Index = props => {
   )
 }
 
-export async function getStaticProps() {
-  // Get external data from the file system, API, DB, etc.
-  const res = await fetch(
-    "http://newsapi.org/v2/everything?" +
-      "q=Manchester United&" +
-      // "from=2020-03-29&" +
-      "sortBy=popularity&" +
-      `apiKey=${process.env.API_KEY}`
-  )
-  const data = await res.json()
-  // The value of the `props` key will be
-  //  passed to the `Home` component
-  return {
-    props: data,
-  }
-}
+// export async function getStaticProps() {
+//   // Get external data from the file system, API, DB, etc.
+//   const res = await fetch(
+//     "http://newsapi.org/v2/everything?" +
+//       "q=Manchester United&" +
+//       // "from=2020-03-29&" +
+//       "sortBy=popularity&" +
+//       `apiKey=${process.env.API_KEY}`
+//   )
+//   const data = await res.json()
+//   // The value of the `props` key will be
+//   //  passed to the `Home` component
+//   return {
+//     props: data,
+//   }
+// }
 
 // Index.getInitialProps = async function () {
 //   const res = await fetch(
